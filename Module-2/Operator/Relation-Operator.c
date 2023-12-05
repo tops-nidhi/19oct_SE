649afa78a92d62bcb8aabc9edbdea166c38ef606
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-12-05 16:04:47 +0530

    05-12-2023

---
+++ Module-2/Operator/Relation-Operator.c
@@ -0,0 +1,13 @@
+/*   >, <, <=, >=, ==, !=    */
+#include<stdio.h>
+int main()
+{
+    int no = 10.7;
+    if(no == 10){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
---
+++ Module-2/Operator/Size-Of.c
@@ -0,0 +1,7 @@
+// Using size of operator we can find the size
+#include<stdio.h>
+int main(){
+    int a = 10;
+    printf("Size of the a is: %d bytes", sizeof(char));
+    return 0;
+}
